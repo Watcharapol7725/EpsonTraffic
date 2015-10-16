994502ce76141c35d7cf61cb5e3538d7dc96f469
watcharapol <w_toms8@hotmail.com>
2015-10-16 11:15:39 +0700

    Button Controller and Push finish

--- app/src/main/java/example/ethkwatcharapol/myapplication/MainActivity.java
+++ app/src/main/java/example/ethkwatcharapol/myapplication/MainActivity.java
@@ -1,7 +1,11 @@
 package example.ethkwatcharapol.myapplication;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -20,10 +24,33 @@ public class MainActivity extends AppCompatActivity {
         //Bind Widget
         bindWidget();
 
+        //button controller
+        buttonController();
+
+
     }   // onCreate
+
+    private void buttonController() {
+
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //sound effect
+                MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.bird_maxican);
+                buttonMediaPlayer.start();
+
+                //Intent to WebView
+                Intent objIntent = new Intent(Intent.ACTION_VIEW);
+objIntent.setData(Uri.parse("https://youtu.be/RgKAFK5djSk"));
+            startActivity(objIntent);
+            } // event
+        });
+
+    }
 
     private void bindWidget() {
         trafficListView = (ListView) findViewById(R.id.listView);
-        aboutMeButton= (Button) findViewById(R.id.button);
+        aboutMeButton = (Button) findViewById(R.id.button);
     }
 } // Main Class นี้คือคลาสหลัก
